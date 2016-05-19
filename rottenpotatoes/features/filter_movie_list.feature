e03367bc5fa65460f098defc97f6df233d00a253
datbuitien-uet <tiendatbui2015@gmail.com>
2016-05-19 16:46:17 +0000

    hw-bdd-cucumber

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -22,11 +22,14 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  When I check the 'PG' and 'R' checkboxes
+  And I uncheck all other checkboxes
+  And I submit
+  And I should see PG and R in ratings colums
+  And I should not see PG-13, G and NC-17 in ratings colums
 
 Scenario: all ratings selected
-  # see assignment
+  When I am on the RottenPotatoes home page
+  When I check the following ratings: PG, R, G, PG-13, NC-17
+  And I submit
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,19 +2,54 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+     Movie.create(movie)
   end
-  fail "Unimplemented"
+
 end
 
+  
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
+When(/^I check the 'PG' and 'R' checkboxes$/) do
+  visit movies_path
+  check('ratings_PG')
+  check('ratings_R')
+end
+
+And (/^I uncheck all other checkboxes$/) do
+  uncheck('ratings_PG-13')
+  uncheck('ratings_NC-17')
+  uncheck('ratings_G')
+end
+
+And (/^I submit$/) do
+  click_button('ratings_submit')
+end
+And (/^I should see PG and R in ratings colums$/) do
+  if page.respond_to? :should
+    page.find('#movies').should have_content("PG")
+    page.find('#movies').should have_content("R")
+  else
+    assert page.find('#movies').has_content?("PG")
+    assert page.find('#movies').has_content?("R")
+  end
+end
+And (/^I should not see PG-13, G and NC-17 in ratings colums$/) do
+  if page.respond_to? :should
+    page.find('#movies').should have_no_content("PG-13")
+    page.find('#movies').should have_no_content("<td>G</td>")
+    page.find('#movies').should have_no_content("NC-17")
+  else
+    assert page.find('#movies').has_no_content?("PG-13")
+    assert page.find('#movies').has_no_content?("<td>G</td>")
+    assert page.find('#movies').has_no_content?("NC-17")
+  end 
+end
+   
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+    ex = e1+" "+e2
+    expect(page).to have_content(ex)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -22,13 +57,19 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck.nil?
+    rating_list.delete!("\"")
+    rating_list.split(",").each do |a|
+      uncheck("ratings_"+a.strip)
+    end
+  else
+    rating_list.delete!("\"")
+    rating_list.split(",").each do |a|
+      uncheck("ratings_"+a.strip)
+    end
+  end
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+   page.should have_css("table#movies tr", :count=>11)
 end
